1986c46f4b8760f8402f60bf2af7d6cd4fa0da75
michaeladilulo <michaeladilulo@gmail.com>
2024-12-21 13:02:18 -0700

    Form complete up through option sets, multi and single entity and operator

--- main.js
+++ main.js
@@ -6,12 +6,58 @@ window.addEventListener('scroll', function() {
     console.log(scrollPosition);
     console.log('offset top', stickySection.offsetTop);
     
-    
-
-    // Check if the sticky section has reached the top of the page
     if (scrollPosition > stickySection.offsetTop || scrollPosition == stickySection.offsetTop) {
         stickySection.classList.add('sticky-active');
     } else {
         stickySection.classList.remove('sticky-active');
     }
+});
+
+document.querySelectorAll('input[name="entityType"]').forEach(function (radioButton) {
+    radioButton.addEventListener('change', function () {
+        const otherInputContainer = document.getElementById('otherInputContainer');
+        if (this.value === "13") {
+            otherInputContainer.style.display = 'block';
+        } else {
+            otherInputContainer.style.display = 'none';
+        }
+    });
+});
+
+document.querySelectorAll('input[name="entityType"]').forEach(function (radioButton) {
+    radioButton.addEventListener('change', function () {
+        const selectedOptionDisplay = document.getElementById('selectedOptionDisplay');
+        const selectedOptionText = document.getElementById('selectedOptionText');
+        const otherInputContainer = document.getElementById('otherInputContainer');
+
+        selectedOptionDisplay.style.display = 'block';
+        selectedOptionText.textContent = this.nextElementSibling.textContent;
+
+        if (this.value === '13') {
+            otherInputContainer.style.display = 'block';
+        } else {
+            otherInputContainer.style.display = 'none';
+        }
+    });
+});
+
+document.querySelectorAll('input[name="operatorType"]').forEach(function (checkbox) {
+    checkbox.addEventListener('change', function () {
+        const selectedOptionsDisplay = document.getElementById('selectedOptionDisplayOperator');
+        const selectedOptionText = document.getElementById('selectedOptionTextOperator');
+        
+        const selectedOptions = [];
+        
+        document.querySelectorAll('input[name="operatorType"]:checked').forEach(function (checkedBox) {
+            const label = checkedBox.nextElementSibling.textContent;
+            selectedOptions.push(label);
+        });
+
+        if (selectedOptions.length > 0) {
+            selectedOptionsDisplay.style.display = 'block';
+            selectedOptionText.innerHTML = selectedOptions.join('<br>');
+        } else {
+            selectedOptionsDisplay.style.display = 'none';
+        }
+    });
 });
